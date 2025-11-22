cd9fe8aaf521c41dd177d4bd7b3ae1cf020b2d9c
Fernando Cesar <fcesarjunior@gmail.com>
2025-11-22 09:19:57 -0300

    Correção da view

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,29 @@
-# views.py
-from rest_framework import permissions
-from rest_framework.decorators import action
+from rest_framework import generics, permissions
+from rest_framework.decorators import action, api_view
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
+from .models import Post
+from .serializers import PostSerializer
+from follows.models import Follow
 
-from posts.models import Post
+class PostListCreateView(generics.ListCreateAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        following_ids = Follow.objects.filter(follower=self.request.user).values_list('followed_id', flat=True)
+        return Post.objects.filter(user__in=following_ids).order_by('-created_at')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Post.objects.filter(user=self.request.user)
 
 class PostInteractionView(GenericViewSet):
     queryset = Post.objects.all()
@@ -26,6 +46,6 @@ class PostInteractionView(GenericViewSet):
         post = self.get_object()
         comment_text = request.data.get('comment')
         if comment_text:
-            post.comments.create(user=request.user, content=comment_text)  # ⚠️ era 'text', mas é 'content'
+            post.comments.create(user=request.user, content=comment_text)
             return Response({'status': 'comment added'})
         return Response({'error': 'No comment text provided'}, status=400)
